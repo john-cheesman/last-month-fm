8606a52a7ea86cf6c35034a429824a05c2b3080b
John Cheesman <info@johncheesman.org.uk>
2014-12-19 12:08:26 +0000

    Get monthly artists

--- src/js/models/top-artists.js
+++ src/js/models/top-artists.js
@@ -9,15 +9,27 @@ config         = require('../config');
 module.exports = Backbone.Epoxy.Model.extend({
     defaults: {
         username: '',
+        topartists: {
+            artist: []
+        },
         fetch: function() {
             return this.fetch();
+        }
+    },
+    computeds: {
+        monthlyArtists: function() {
+            var artists;
+
+            artists = this.get('topartists');
+
+            return artists.artist.length;
         }
     },
     urlRoot: 'http://ws.audioscrobbler.com/2.0/?',
     url: function() {
         return this.urlRoot +
             'method=user.gettopartists&user=' +
-            this.defaults.username +
+            this.get('username') +
             '&limit=200' +
             '&period=1month' +
             '&api_key=' +

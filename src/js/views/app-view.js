1444b3f57fe24ac483783d95b284048a56d7f18a
John Cheesman <info@johncheesman.org.uk>
2014-12-20 19:52:24 +0000

    Set username properties on user search and fetch

--- src/js/views/app-view.js
+++ src/js/views/app-view.js
@@ -1,9 +1,12 @@
-var Backbone, $, app, User;
+var Backbone, $, app, User, TopArtists, UserView, TopArtistsView;
 
 $              = require('jquery');
 Backbone       = require('backbone');
 Backbone.$     = $;
 User           = require('../models/user');
+TopArtists     = require('../models/top-artists');
+UserView       = require('../views/user-view');
+TopArtistsView = require('../views/top-artists-view');
 
 module.exports = Backbone.View.extend({
     el: '.js-app',
@@ -14,9 +17,18 @@ module.exports = Backbone.View.extend({
 
     initialize: function() {
         this.user = new User();
+        this.topArtists = new TopArtists();
+
+        this.userView = new UserView({
+            model: this.user
+        });
+
+        this.topArtistsView = new TopArtistsView({
+            model: this.topArtists
+        });
 
         this.input = this.$('.js-user-search-input');
-        console.log(this.user);
+        console.log(this.user, this.topArtists, this.userView, this.topArtistsView);
     },
 
     render: function() {
@@ -28,6 +40,10 @@ module.exports = Backbone.View.extend({
         if (!this.input.val()) return;
 
         this.user.set('username', this.input.val());
+        this.topArtists.set('username', this.input.val());
+
+        this.user.fetch();
+        this.topArtists.fetch();
     }
 });
 
